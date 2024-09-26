0dd6d524a3d9db9d360e8d453393fc610a7fc42a
tonydevpro <antonio.1358@hotmail.com>
2024-09-26 17:01:15 -0300

    separaçao

---
+++ script.js
@@ -0,0 +1,105 @@
+var elementos = document.querySelectorAll('.player-options div > img')
+        var playerOpt = '';
+        var inimigoOpt = '';
+
+
+        function validarVitoria(){
+
+            let vencedor = document.querySelector('.vencedor');
+
+            if(playerOpt == 'papel'){
+
+                if(inimigoOpt == 'papel'){
+                    //empate
+                    vencedor.innerHTML = 'O jogo foi empatado'
+                }
+                else if(inimigoOpt == 'tesoura'){
+                    //inimigo win
+                    vencedor.innerHTML = 'O inimigo ganhou'
+                }
+                else if(inimigoOpt == 'pedra'){
+                    //play win
+                    vencedor.innerHTML = 'O player ganhou'
+                }
+    
+            }
+
+            if(playerOpt == 'pedra'){
+
+                if(inimigoOpt == 'papel'){
+                    
+                    vencedor.innerHTML = 'O inimigo ganhou'
+                }
+                else if(inimigoOpt == 'tesoura'){
+                    
+                    vencedor.innerHTML = 'O player ganhou'
+                }
+                else if(inimigoOpt == 'pedra'){
+                    
+                    vencedor.innerHTML = 'O jogo foi empatado'
+                }
+
+            }
+
+            if(playerOpt == 'tesoura'){
+
+                if(inimigoOpt == 'papel'){
+                    
+                    vencedor.innerHTML = 'O player ganhou'
+                }
+                else if(inimigoOpt == 'tesoura'){
+                    
+                    vencedor.innerHTML = 'O jogo foi empatado'
+                }
+                else if(inimigoOpt == 'pedra'){
+                    
+                    vencedor.innerHTML = 'O inimigo ganhou'
+            }
+
+            }
+
+        }
+
+        function resetInimigo(){
+            const enemyOptions = document.querySelectorAll('.enemy-options div');
+
+            for(var i = 0; i < enemyOptions.length; i++){
+                enemyOptions[i].childNodes[0].style.opacity = 0.3;
+            }
+        }
+
+        function inimigoJogar(){
+            let rand = Math.floor(Math.random()*3);
+
+            const enemyOptions = document.querySelectorAll('.enemy-options div');
+            resetInimigo();
+            for(var i = 0; i < enemyOptions.length; i++){
+                if(i == rand){
+                    enemyOptions[i].childNodes[0].style.opacity = 1;
+                    inimigoOpt = enemyOptions[i].childNodes[0].getAttribute('opt');
+                }
+            };
+        
+
+            validarVitoria();
+
+            
+        }
+
+
+        function resetOpacityPlayer(){
+            for(var i = 0; i < elementos.length; i++){
+                elementos[i].style.opacity = 0.3;
+            }
+        }
+
+
+        for(var i = 0; i<elementos.length; i++){
+            elementos[i].addEventListener('click', function(t){
+                resetOpacityPlayer();
+                t.target.style.opacity = 1;
+                playerOpt = t.target.getAttribute('opt');
+
+               inimigoJogar();
+            });
+        }
